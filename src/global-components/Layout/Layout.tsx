ee9c7bb7efaec197ae8f29a936003b5097977eb6
seko <kochnev_serezhenka14@mail.ru>
2025-04-16 12:43:11 +0500

    add permissions

--- src/global-components/Layout/Layout.tsx
+++ src/global-components/Layout/Layout.tsx
@@ -7,7 +7,7 @@ import {
 } from '@ant-design/icons';
 import { Button, Flex, Layout } from 'antd';
 import { Navigate, Outlet, useNavigate } from 'react-router-dom';
-import { AppMenu } from './components/menu/Menu';
+import { AppMenu } from './components/Menu/Menu';
 import styles from './Layout.module.css';
 import { useLogoutMutation } from '../../app-service/services/auth';
 import { IUser } from '../../utils/userType';
@@ -15,7 +15,7 @@ import { IUser } from '../../utils/userType';
 const { Header, Sider, Content } = Layout;
 
 export const LayoutPage: React.FC = () => {
-    const user: IUser = JSON.parse(localStorage.getItem('user') || '');
+    const user: IUser = JSON.parse(localStorage.getItem('user') || '{}');
 
     const [collapsed, setCollapsed] = useState(false);
     const [isMobile, setIsMobile] = useState(window.innerWidth < 768); // Проверка на мобильный экран
@@ -110,11 +110,7 @@ export const LayoutPage: React.FC = () => {
                             className={styles['trapezoid-button']}
                         >
                             <div className={styles['trapezoid-button__icon']}>
-                                {collapsed ? (
-                                    <RightOutlined />
-                                ) : (
-                                    <LeftOutlined />
-                                )}
+                                {collapsed ? <RightOutlined /> : <LeftOutlined />}
                             </div>
                         </div>
                     )}
