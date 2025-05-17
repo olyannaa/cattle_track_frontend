39c9b6e2517819428bbb28a9aefe2b1fbcdf1a86
Arina Topor <arinatopor@abanking.ru>
2025-05-17 23:40:44 +0500

    add logo

--- src/app/authorization/AuthorizationPage.tsx
+++ src/app/authorization/AuthorizationPage.tsx
@@ -3,6 +3,7 @@ import styles from './AuthorizationPage.module.css';
 import { Login } from './components/Login/Login';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import logo from '../../assets/header-logo.svg';
 
 export const Authorization = () => {
     const navigate = useNavigate();
@@ -16,7 +17,7 @@ export const Authorization = () => {
     return (
         <Flex className={styles.wrapper} align='center' justify='center'>
             <Flex className={styles.header}>
-                <Flex className={styles.header__logo}>Logo</Flex>
+                <Flex className={styles.header__logo}><img width={124} src={logo}/></Flex>
             </Flex>
             <Flex className={styles.body} align='center' justify='center'>
                 <Login />
--- src/global-components/Layout/Layout.tsx
+++ src/global-components/Layout/Layout.tsx
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import {
-    AppstoreFilled,
-    LeftOutlined,
-    RightOutlined,
-    UserOutlined,
-} from '@ant-design/icons';
+import { AppstoreFilled, LeftOutlined, RightOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Flex, Layout } from 'antd';
 import { Navigate, Outlet, useNavigate } from 'react-router-dom';
-import { AppMenu } from './components/Menu/Menu';
+import { AppMenu } from './components/menu/Menu';
 import styles from './Layout.module.css';
 import { useLogoutMutation } from '../../app-service/services/auth';
 import { IUser } from '../../utils/userType';
+import logo from '../../assets/header-logo.svg';
 
 const { Header, Sider, Content } = Layout;
 
@@ -33,6 +29,7 @@ export const LayoutPage: React.FC = () => {
         background: '#ffffff',
         maxHeight: 696,
         display: isMobile && collapsed ? 'none' : 'block',
+        zIndex: 1,
     };
 
     useEffect(() => {
@@ -71,6 +68,7 @@ export const LayoutPage: React.FC = () => {
                         display: 'flex',
                         justifyContent: 'space-between',
                         alignItems: 'center',
+                        width: '100%'
                     }}
                 >
                     <div className={styles['header__content-logo']}>
@@ -85,7 +83,7 @@ export const LayoutPage: React.FC = () => {
                                 }}
                             />
                         )}
-                        <div>LOGO</div>
+                        <img width={124} src={logo}/>
                     </div>
                     <Flex gap={'4px'}>
                         <Button type={'text'}>
