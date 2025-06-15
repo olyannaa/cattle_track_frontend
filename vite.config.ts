b28869a3359d2e4d0e49cadde9c771ccd56fea17
Arina Topor <arinatopor@abanking.ru>
2025-06-15 19:31:34 +0500

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+            },
+            includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'Cattle Track',
+                short_name: 'CattleTrack',
+                description: 'Сервис учета КРС',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ],
+            },
+        }),
+    ],
+});
